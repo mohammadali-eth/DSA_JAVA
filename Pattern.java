5b5ca7eedebf51fef2f259be3b21885d6a9a0305
MohammadAli <109208273+MohammadAli440@users.noreply.github.com>
2026-02-09 23:40:59 +0530

    3,4,5,6 question complete

--- Pattern.java
+++ Pattern.java
@@ -17,7 +17,7 @@ public class Pattern {
         //pattern 2;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("*"); // 
             }
             System.out.println();
         }
@@ -26,7 +26,7 @@ public class Pattern {
         //pattern 3;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(j); //j is used to create a number pyramid in which the numbers will be printed one after the other.
             }
             System.out.println();
         }
@@ -35,23 +35,29 @@ public class Pattern {
         //pattern 4;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i);
+                System.out.print(i); //i is used to create a number pyramid in which one number is printed once and another number is printed twice.
             }
             System.out.println();
         }
+        System.out.println();
+
+        //pattern 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        //pattern 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(j); //j is used to create a number pyramid in which the numbers will be printed one after the other.
+            }
+            System.out.println();
+        }
+        System.out.println();
 
     }
 }
-// pattern 5;
-// public class Pattern {
-//     public static void main(String[] args) {
-//         int n = 5;
-//         for (int i = 0; i <= n; i++) {
-//             for (int j = 1; j <= n - i; j++) {
-//                 System.out.print("* ");
-//             }
-//             System.out.println();
-//         }
-//     }
-// }
-
