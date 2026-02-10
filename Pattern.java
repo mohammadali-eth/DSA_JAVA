6b3ca667a01290868e6e43278d7d2e1ceb1e4dee
MohammadAli <109208273+MohammadAli440@users.noreply.github.com>
2026-02-10 07:49:33 +0530

    7 completed

--- Pattern.java
+++ Pattern.java
@@ -60,17 +60,18 @@ public class Pattern {
         System.out.println();
 
         //pattern 7;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(" ");
-                for (int k = 1; k <= 2 * i + 1; k++) {
-                    System.out.print("* ");
-                }
-                for (int k = 1; k <= n - i + 1; k++) {
-                    System.out.print(" ");
-                }
-                System.out.println();
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("  ");
             }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("* ");
+            }
+            for (int k = 0; k < n - i - 1; k++) {
+                System.out.print("  ");
+            }
+            System.out.println();
 
         }
     }
